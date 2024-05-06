0061db3b82d50ae48c5f7b3726e1bbe3f06b022a
AliGadoo <aligado232@gmail.com>
2024-05-06 23:00:44 +0200

    add cart tab

--- components/productsList.tsx
+++ components/productsList.tsx
@@ -3,14 +3,22 @@ import { FlatList, StyleSheet, View } from "react-native";
 import ProductCard from "./productCard";
 import { ProductsData } from "@/lib/dao/products";
 
-const ProductsList = ({ products }: { products: ProductsData[] }) => {
+const ProductsList = ({
+  products,
+  deleteProduct,
+}: {
+  products: ProductsData[];
+  deleteProduct?: () => void;
+}) => {
   return (
     <View style={styles.container}>
       <FlatList
         contentContainerStyle={styles.listContent}
         data={products}
         renderItem={({ item }) => {
-          return <ProductCard productData={item} />;
+          return (
+            <ProductCard productData={item} deleteProduct={deleteProduct} />
+          );
         }}
       />
     </View>
