5162126ed95911ad0c637da283729db89d9fc8ff
AliGadoo <aligado232@gmail.com>
2024-05-06 11:44:19 +0200

    added ProductsDao class

--- app/(app)/(customer-tabs)/index.tsx
+++ app/(app)/(customer-tabs)/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, View , Text} from 'react-native';
-
+import { productDao } from '@/lib/dao/products';
 const Home = () => {
+    console.log(productDao.getAll());
     return (
         <View>
             <Text>i will fetch the data later</Text>
