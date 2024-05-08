b5ecfef14445819140d19f47e9458863a33a66db
Ahmed <ahmedshimy64@gmail.com>
2024-05-09 02:01:36 +0300

    Edit profile

--- app/(app)/(customer-tabs)/profile.tsx
+++ app/(app)/(customer-tabs)/profile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, View, Image, Text, ImageBackground } from "react-native";
-import { Feather } from "@expo/vector-icons";
-import { Entypo } from "@expo/vector-icons";
+import Customer from "@/components/Details/Customer";
+
 import { useContext } from "react";
 import { userContext } from "@/lib/userContext";
 import colors from "@/lib/colors";
@@ -12,87 +12,20 @@ const Profile = () => {
     ? user.avatar
     : "https://static.vecteezy.com/system/resources/previews/026/966/960/non_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg";
 
-  if (user.storeName) {
-    return (
-      <View style={styles.container}>
-        <View style={{ flexDirection: "row" }}>
-          <Image style={styles.image} source={{ uri: image }}></Image>
-          <Entypo
-            name="camera"
-            size={24}
-            color="black"
-            style={{ marginTop: 150 }}
-          />
-        </View>
-
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Text style={{ fontSize: 22 }}>
-            Store Name:<Text style={{ fontSize: 16 }}> {user.storeName}</Text>
-          </Text>
-          <Feather name="edit-2" size={24} color="black" />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            maxWidth: 300,
-            width: 300,
-            maxHeight: 70,
-          }}
-        >
-          <Text style={{ fontSize: 22, alignSelf: "flex-start" }}>
-            Address:<Text style={{ fontSize: 14 }}> {user.address}</Text>
-          </Text>
-          <Feather name="edit-2" size={24} color="black" />
-        </View>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <View style={{ flexDirection: "row" }}>
-          <Image style={styles.image} source={{ uri: image }}></Image>
-          <Entypo
-            name="camera"
-            size={24}
-            color="black"
-            style={{ marginTop: 150 }}
-          />
-        </View>
-
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Text style={{ fontSize: 22 }}>
-            Name:
-            <Text style={{ fontSize: 16 }}>
-              {" "}
-              {user.firstName + " " + user.lastName + " "}
-            </Text>
-          </Text>
-          <Feather name="edit-2" size={24} color="black" />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            maxWidth: 300,
-            width: 300,
-            maxHeight: 70,
-          }}
-        >
-          <Text style={{ fontSize: 22, alignSelf: "flex-start" }}>
-            Address:<Text style={{ fontSize: 14 }}> {user.address}</Text>
-          </Text>
-          <Feather name="edit-2" size={24} color="black" />
-        </View>
-      </View>
-    );
-  }
+  return (
+    <Customer
+      first={user.firstName!}
+      last={user.lastName!}
+      address={user.address}
+      image={image}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: "100%",
+    width: 90,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: colors.primary,
