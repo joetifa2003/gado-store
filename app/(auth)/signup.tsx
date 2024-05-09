97eeafc9e65e4f8f2212d6bb1c03b80a0d843a1a
Ahmed <ahmedshimy64@gmail.com>
2024-05-09 19:51:28 +0300

    Edit SignUp UI

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,28 +1,64 @@
 import Button from "@/components/button";
 import { Stack, useRouter } from "expo-router";
-import { View } from "react-native";
+import { View, Image, Pressable, StyleSheet } from "react-native";
+import colors from "@/lib/colors";
 
 export default function SingUp() {
   const router = useRouter();
   return (
-    <View
-      style={{
-        display: "flex",
-        width: "100%",
-        height: "100%",
-        flexDirection: "column",
-      }}
-    >
+    <View style={styles.container}>
       <Stack.Screen options={{ title: "Sign up" }} />
 
-      <Button
-        title="Provider"
-        onPress={() => router.navigate("/signup-provider")}
-      />
-      <Button
-        title="Customer"
-        onPress={() => router.navigate("/signup-customer")}
-      />
+      <Pressable onPress={() => router.navigate("/signup-provider")}>
+        <View style={styles.component}>
+          <Image
+            style={styles.image}
+            source={require("@/assets/images/businessman-icon-png-12.png")}
+          ></Image>
+          <Button
+            title="Provider"
+            onPress={() => router.navigate("/signup-provider")}
+          ></Button>
+        </View>
+      </Pressable>
+
+      <Pressable onPress={() => router.navigate("/signup-customer")}>
+        <View style={styles.component}>
+          <Image
+            style={styles.image}
+            source={require("@/assets/images/Customer.jpg")}
+          ></Image>
+          <Button
+            title="Customer"
+            onPress={() => router.navigate("/signup-customer")}
+          ></Button>
+        </View>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 50,
+  },
+  image: {
+    width: 170,
+    height: 170,
+    backgroundColor: colors.white,
+    margin: 10,
+    borderRadius: 30,
+  },
+  component: {
+    backgroundColor: colors.dark,
+    borderRadius: 20,
+    paddingBottom: 20,
+    padding:10
+    
+  },
+});
